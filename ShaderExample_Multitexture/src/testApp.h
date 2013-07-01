cfbcced4eda6a10bdfd478162d219cf987640289
Jason Van Cleave <jvcleave@gmail.com>
2013-07-01 05:04:06 -0400

    init

---
+++ ShaderExample_Multitexture/src/testApp.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "ofMain.h"
+
+#include "FPSLogger.h".
+
+class testApp : public ofBaseApp{
+
+	public:
+
+		void setup();
+		void update();
+		void draw();
+		
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);		
+		
+	ofImage tex0Source;
+	ofImage tex1Source;
+	
+	ofTexture tex0;
+	ofTexture tex1;
+	
+	float drawWidth;
+	float drawHeight;
+	
+	ofShader shader;
+	ofFbo fbo;
+
+	FPSLogger fpsLogger;
+};
+
